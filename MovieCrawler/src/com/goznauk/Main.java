736cadcf3c7a4c1bb1ec7f3cdd268122d3be8f50
goznauk <goznauk@nhnnext.org>
2014-12-01 13:07:09 +0900

    asdf

--- MovieCrawler/src/com/goznauk/Main.java
+++ MovieCrawler/src/com/goznauk/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static final boolean isActor = false;
 
     public static void main(String[] args) {
-      FriendFinder friendFinder = new FriendFinder(13916, 108226); // 최진안 : 백설희, 다나
+    //  FriendFinder friendFinder = new FriendFinder(13916, 108226); // 최진안 : 백설희, 다나
     //  FriendFinder friendFinder = new FriendFinder(119301, 43015); // 신상걸 : 권영팔, 김희수
-    //  FriendFinder friendFinder = new FriendFinder(16834, 356524); // 이호창 : 황남, 민진웅
+      FriendFinder friendFinder = new FriendFinder(16834, 356524); // 이호창 : 황남, 민진웅
     //  FriendFinder friendFinder = new FriendFinder(324964, 57659); // 최훈존 : 김소영, 정덕순
     //  FriendFinder friendFinder = new FriendFinder(41324, 134826); // 박영재 : 김삼화, 김수현
     //  FriendFinder friendFinder = new FriendFinder(311452, 64596); // 백상호 : 김이안, 이문수
